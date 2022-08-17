21f4fdbe7785a64625343b7f29affa57d3739f40
jodafons <jodafons@lps.ufrj.br>
2022-08-17 19:18:36 +0000

    updaye

--- docker/dns-server/run.sh
+++
@@ -1,3 +0,0 @@
-sudo docker-compose down
-sudo docker rm -f $(docker ps -a -q)
-sudo docker-compose up -d
---
+++ servers/dns-server/install.sh
@@ -0,0 +1,23 @@
+# install bind9
+sudo apt-get update
+sudo apt-get install -y bind9 bind9utils bind9-doc
+sudo mkdir -p /etc/bind/zones
+source update.sh
+
+# copy configuration
+echo 's/OPTIONS=.*/OPTIONS="-4 -u bind"/' > bind9
+sudo mv bind9 /etc/default
+sudo cp zones/db.external /etc/bind/zones/
+sudo cp zones/db.private /etc/bind/zones/
+sudo cp zones/db.lps.ufrj.br /etc/bind/zones/
+sudo cp zones/db.sandbox /etc/bind/zones/
+sudo cp named.conf.options /etc/bind/
+sudo cp named.conf.local /etc/bind/
+
+named-checkconf
+named-checkzone sandbox.ufrj.br /etc/bind/zones/db.sandbox
+
+systemctl restart bind9
+sudo apt install -y ufw
+sudo ufw allow Bind9
+
