13c51627e7d1565644ad8d4a66f94c93678374b6
jodafons <jodafons@lps.ufrj.br>
2022-08-19 13:14:10 +0000

    update

--- servers/ldap-server/install_kerberos.sh
+++ servers/ldap-server/install_kerberos.sh
@@ -3,13 +3,15 @@
 sudo apt install -y vim resolvconf
 
 # For LPS this should be 146.164.147.2
-echo 'nameserver 192.168.0.200' > head
+echo 'nameserver 146.164.147.2
+search lps.ufrj.br' > head
 sudo mv head /etc/resolvconf/resolv.conf.d/
 sudo service resolvconf restart
 
 sudo dpkg-reconfigure debconf # Dialog and Low priority as input in textbox
-
 sudo apt install -y krb5-{admin-server,kdc}
+
+krb5_newrealm
 
 
 
