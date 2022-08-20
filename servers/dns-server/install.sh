f7e44c33f61f93e297a497a70a295ca492ce23c9
jodafons <jodafons@lps.ufrj.br>
2022-08-20 16:38:09 +0000

    update

--- servers/dns-server/install.sh
+++ servers/dns-server/install.sh
@@ -2,16 +2,11 @@
 sudo apt-get update
 sudo apt-get install -y net-tools bind9 bind9utils bind9-doc
 sudo mkdir -p /etc/bind/zones
-source update.sh
 
 # copy configuration
 echo 's/OPTIONS=.*/OPTIONS="-4 -u bind"/' > bind9
 sudo mv bind9 /etc/default
-sudo cp zones/* /etc/bind/zones/
-sudo cp named.conf.local /etc/bind/
-
-named-checkconf
-named-checkzone sandbox.ufrj.br /etc/bind/zones/db.sandbox
+source update.sh
 
 systemctl restart bind9
 sudo apt install -y ufw
---
+++ servers/dns-server/setup_network.sh
@@ -0,0 +1,19 @@
+echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/$USER
+sudo apt install net-tools
+
+
+# get name with ifconfig 
+echo "network:
+  version: 2
+  ethernets:
+     ens18:
+        dhcp4: false
+        addresses: [146.164.147.2/24]
+        gateway4: 146.164.147.1
+        nameservers:
+          addresses: [8.8.8.8, 8.8.4.4]" > 00-installer-config.yaml
+
+sudo mv 00-installer-config.yaml /etc/netplan 
+sudo netplan apply
+ifconfig
+
