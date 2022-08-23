e1a11f651a4553a67b759c0e5a8a98134415fea9
jodafons <jodafons@lps.ufrj.br>
2022-08-23 23:22:48 +0000

    update

--- servers/auth-server/install_kerberos.sh
+++ servers/auth-server/install_kerberos.sh
@@ -1,5 +1,6 @@
 
 
+# install kerberos
 sudo dpkg-reconfigure debconf # Dialog and Low priority as input in textbox
 sudo apt install -y krb5-{admin-server,kdc}
 sudo krb5_newrealm
@@ -11,4 +12,13 @@ chmod -R 750  /var/log/kerberos
 invoke-rc.d krb5-kdc restart
 invoke-rc.d krb5-admin-server restart
 
-
+# setup kerberos accounts and polices
+kadmin.local -q "add_policy -minlength 8 -minclasses 3 admin"
+kadmin.local -q "add_policy -minlength 8 -minclasses 4 host"
+kadmin.local -q "add_policy -minlength 8 -minclasses 4 service"
+kadmin.local -q "add_policy -minlength 8 -minclasses 2 user"
+kadmin.local -q "addprinc -policy admin root/admin"
+kadmin.local -q "addprinc -policy user unprivileged_user"
+kadmin.local -q "ank -policy admin root/admin"
+kadmin.local -q "ank -policy user joao.pinto"
+kadmin.local -q "list_principals"
