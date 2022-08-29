94d4df6fba4ebf2b70e3d49b94478462c6c72e06
jodafons <jodafons@lps.ufrj.br>
2022-08-29 19:20:41 -0300

    update

--- nodes/caloba_cpu/install_base.sh
+++ nodes/caloba_cpu/install_base.sh
@@ -1,4 +1,14 @@
 
+#
+# Append market_place into the mount
+#
+
+apt install -y nfs-common
+mkdir /mnt/market_place
+echo "10.1.1.202:/volume1/market_place /mnt/market_place nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+
+
+
 
 # install kerberos
 apt install -y krb5-config krb5-user
