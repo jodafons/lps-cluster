29e0492d4bda05bb8c82bdcc53dffecd8bd10fa6
jodafons <jodafons@lps.ufrj.br>
2022-08-29 18:04:28 -0300

    update

--- nodes/install_modules.sh
+++ nodes/install_modules.sh
@@ -14,3 +14,8 @@ ln -s /usr/share/Modules/init/profile.sh /etc/profile.d/modules.sh
 ln -s /usr/share/Modules/init/profile.csh /etc/profile.d/modules.csh
 source /usr/share/Modules/init/bash
 cd $basedir
+
+
+# setup modules
+mkdir /etc/modulefiles/cuda
+cp modules/cuda/11.4 /etc/modulefiles/cuda
