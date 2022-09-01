a840101517398adcdf149798345e1bcd87d35031
jodafons <jodafons@lps.ufrj.br>
2022-08-31 23:15:23 -0300

    update

--- servers/slurm-server/install_base.sh
+++ servers/slurm-server/install_base.sh
@@ -24,7 +24,7 @@ chmod -R 700 /etc/pam_scripts
 chown -R root:root /etc/pam_scripts
 cp conf_files/login-logger.sh /etc/pam_scripts
 chmod +x /etc/pam_scripts/login-logger.sh
-echo "session required pam_exec.so /etc/pam_scripts/login-logger.sh" >> /etc/pam.d/sshd
+#echo "session required pam_exec.so /etc/pam_scripts/login-logger.sh" >> /etc/pam.d/sshd
 
 
 # configure NTP
