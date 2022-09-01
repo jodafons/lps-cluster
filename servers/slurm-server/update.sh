9e1cf152036823fee990bd6a4ff4317946bcde07
jodafons <jodafons@lps.ufrj.br>
2022-09-01 13:12:15 -0300

    update

--- servers/slurm-server/update.sh
+++ servers/slurm-server/update.sh
@@ -1,21 +1,21 @@
 
-# update SLURM
-cp slurm/slurm.conf /etc/slurm/
-cp slurm/slurmdbd.conf /etc/slurm
+# recopy everything to SLURM
+cp files/slurm/slurm.conf /etc/slurm/
+cp files/slurm/slurmdbd.conf /etc/slurm
 chmod 600 /etc/slurm/slurmdbd.conf
 chown -R slurm /etc/slurm
-cp slurm/slurmdbd.service /etc/systemd/system/
-cp slurm/slurmctld.service /etc/systemd/system/
-cp slurm/slurm.conf /mnt/market_place/slurm_build
+cp files/slurm/slurmdbd.service /etc/systemd/system/
+cp files/slurm/slurmctld.service /etc/systemd/system/
+cp files/slurm/slurm.conf /mnt/market_place/slurm_build
 
-
+# restart all services
+systemctl start munge
 systemctl daemon-reload
-systemctl enable slurmdbd
 systemctl start slurmdbd
-systemctl enable slurmctld
 systemctl start slurmctld
 
 
-
 # check slurm
-systemctl status slurmctld
+#systemctl status slurmdbd
+#systemctl status slurmctld
+scontrol reconfigure
