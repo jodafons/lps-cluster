1f51de3bed989ad01c5ca711ab5098f61ea686d9
jodafons <jodafons@lps.ufrj.br>
2022-09-10 19:35:24 -0300

    update

--- rancher/rancher.sh
+++ rancher/rancher.sh
@@ -1,5 +1,3 @@
 
 # setup rancher server
-docker run -d --restart=unless-stopped -p 8080:8080 rancher/server:v2.4.8
-
-
+docker run -d --name=rancher-server --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:v2.4.8
