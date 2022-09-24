4feb20fa77b95628675d83b09e72fd88895f8a72
cluster <cluster@rancher-server.lps.ufrj.br>
2022-09-23 22:01:25 -0300

    update

--- kubernetes/install.sh
+++ kubernetes/install.sh
@@ -1,4 +1,4 @@
 
-
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible-playbook -i hosts tasks/users.yaml
 ansible-playbook -i hosts tasks/install_k8s.yaml
--- kubernetes/ping.sh
+++ kubernetes/ping.sh
@@ -1,1 +1,2 @@
+export ANSIBLE_HOST_KEY_CHECKING=False
 ansible all -m ping -v -i hosts
