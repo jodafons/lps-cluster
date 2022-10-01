997deea1daae8e224bb9f9b52520f84aa7ee6a29
jodafons <jodafons@lps.ufrj.br>
2022-10-01 08:49:52 -0300

    update

--- kubernetes/install_base.sh
+++ kubernetes/install_base.sh
@@ -30,3 +30,27 @@ EOF
 sudo sysctl --system
 
 
+
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+
+
+
+kubectl create namespace cert-manager
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.7.1
+
+
+helm install rancher rancher-stable/rancher \
+  --namespace cattle-system \
+  --set hostname=kube-master.lps.ufrj.br \
+  --set bootstrapPassword=admin
+
+
