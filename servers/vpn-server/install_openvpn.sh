d0d013402c2c1309c2a21beb39575e7e61387807
jodafons <jodafons@lps.ufrj.br>
2022-10-03 18:50:18 -0300

    update

--- servers/vpn-server/install_openvpn.sh
+++ servers/vpn-server/install_openvpn.sh
@@ -1,6 +1,6 @@
 
 
-docker-compose run --rm openvpn ovpn_genconfig -u udp://146.164.147.6
+docker-compose run --rm openvpn ovpn_genconfig -u udp://146.164.147.6:3000
 docker-compose run --rm openvpn ovpn_initpki
 sudo chown -R $(whoami): $HOME/openvpn-data
 docker-compose up -d openvpn
