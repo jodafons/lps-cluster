374e73c23507daf224e66be60135524c1a503300
jodafons <jodafons@lps.ufrj.br>
2022-10-07 21:07:45 -0300

    update

--- kubernetes/kube_join.sh
+++
@@ -1,5 +0,0 @@
-
-MASTERNODE_TOKEN=$1
-MASTERNODE_IP=146.164.147.31
-
-curl -sfL https://get.k3s.io | K3S_URL=https://$MASTERNODE_IP:6443 K3S_TOKEN=$MASTERNODE_TOKEN sh - --docker
--- kubernetes/rancher.sh
+++
@@ -1,3 +0,0 @@
-
-# setup rancher server
-docker run -d --name=rancher-server --restart=unless-stopped -p 80:80 -p 443:443 --privileged rancher/rancher:v2.4.8
