ea7e4f266cd4425d3f46d858054393a71cc04133
jodafons <jodafons@lps.ufrj.br>
2022-10-07 21:10:12 -0300

    update

--- servers/dns-server/install.sh
+++ servers/dns-server/install.sh
@@ -11,3 +11,12 @@ systemctl restart bind9
 sudo apt install -y ufw
 sudo ufw allow Bind9
 
+
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:fish:release:3.list
+curl -fsSL https://download.opensuse.org/repositories/shells:fish:release:3/Debian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_fish_release_3.gpg > /dev/null
+sudo apt update
+sudo apt install -y fish
+which fish
+echo 'fish' >> /home/cluster/.bashrc
+
+
