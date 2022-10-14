e0ec5f0074f81ba9fb3efce64b627ef7a3a72167
jodafons <jodafons@lps.ufrj.br>
2022-10-14 17:12:40 -0300

    update

--- servers/auth-server/backup_ldap.sh
+++ servers/auth-server/backup_ldap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-slapcat -b cn=config > config.ldif
-slapcat -b dc=example,dc=com > lps.ufrj.br.ldif
+sudo slapcat -b cn=config > config.ldif
+sudo slapcat -b dc=lps,dc=ufrj,dc=br > lps.ufrj.br.ldif
 #chown root:root ${BACKUP_PATH}/*
 #chmod 600 ${BACKUP_PATH}/*.ldif
