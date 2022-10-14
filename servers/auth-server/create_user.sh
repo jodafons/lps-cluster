b9b9bd288c446e8477171483922b83234019a219
jodafons <jodafons@lps.ufrj.br>
2022-10-14 20:54:42 -0300

    update

--- servers/auth-server/create_user.sh
+++ servers/auth-server/create_user.sh
@@ -1,4 +1,4 @@
 master_password=$1
 username=$2
 
-kadmin -q "ank -policy user -pw changemenow -needchange $username " -w $master_password
+sudo kadmin -q "ank -policy user -pw changemenow -needchange $username " -w $master_password
