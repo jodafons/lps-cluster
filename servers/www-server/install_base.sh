2d97e1d79836c6e0aef06bd330b97f2d84bf3b85
jodafons <jodafons@lps.ufrj.br>
2022-10-14 15:13:05 -0300

    update

--- servers/www-server/install_base.sh
+++ servers/www-server/install_base.sh
@@ -11,7 +11,7 @@ apt install -y nfs-common
 mkdir /mnt/market_place
 mkdir /mnt/home
 echo "10.1.1.202:/volume1/market_place /mnt/market_place nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
-echo "10.1.1.202:/volume1/homes /mnt/home nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+echo "10.1.1.202:/volume1/homes /mnt/homes nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
 
 
 #
