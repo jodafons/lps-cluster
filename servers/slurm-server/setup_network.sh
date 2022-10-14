e1b513458677985d2b11272da29caeaf6750ab57
jodafons <jodafons@lps.ufrj.br>
2022-10-14 18:02:02 -0300

    update

--- servers/slurm-server/setup_network.sh
+++ servers/slurm-server/setup_network.sh
@@ -36,17 +36,6 @@ search lps.ufrj.br' > /etc/resolvconf/resolv.conf.d/head
 service resolvconf restart
 
 
-
-#
-# Append market_place into the mount
-#
-
-apt install -y nfs-common
-mkdir /mnt/market_place
-echo "10.1.1.202:/volume1/market_place /mnt/market_place nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
-
-
-# others
-apt install -y htop sshpass
+reboot now
 
 
