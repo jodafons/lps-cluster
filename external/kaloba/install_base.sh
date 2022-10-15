2d33dd7c73b762fec08f15369af5a4757deb1ed7
jodafons <jodafons@lps.ufrj.br>
2022-10-15 20:30:33 -0300

    update

---
+++ external/kaloba/install_base.sh
@@ -0,0 +1,39 @@
+#
+# Install dependencies
+#
+apt install -y htop vim git sshpass curl wget 
+
+
+#
+# Setup NFS
+#
+apt install -y nfs-common
+mkdir /mnt/kubernetes
+echo "10.1.1.202:/volume1/kubernetes /mnt/kubernetes nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+
+
+#
+# Install docker
+#
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
+groupadd docker
+gpasswd -a root docker
+gpasswd -a cluster docker
+
+
+reboot now
