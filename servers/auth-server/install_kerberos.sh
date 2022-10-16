04661c7d0b166223620356721e97a1c12e74826a
jodafons <jodafons@lps.ufrj.br>
2022-10-15 22:06:43 -0300

    update

--- servers/auth-server/install_kerberos.sh
+++ servers/auth-server/install_kerberos.sh
@@ -4,8 +4,8 @@
 sudo dpkg-reconfigure debconf # Dialog and Low priority as input in textbox
 sudo apt install -y krb5-{admin-server,kdc}
 sudo krb5_newrealm
-sudo cp kadm5.acl /etc/krb5kdc/kadm5.acl
-sudo cp krb5.conf /etc/krb5.conf
+sudo cp files/kadm5.acl /etc/krb5kdc/kadm5.acl
+sudo cp files/krb5.conf /etc/krb5.conf
 mkdir /var/log/kerberos
 touch /var/log/kerberos/{krb5kdc,kadmin,krb5lib}.log
 chmod -R 750  /var/log/kerberos
