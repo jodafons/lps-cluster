ac15f5eb8f220d53a9d8069939a43a37e35b9000
jodafons <jodafons@lps.ufrj.br>
2022-10-16 12:03:30 -0300

    update

--- external/kaloba-cluster/install_base.sh
+++ external/kaloba-cluster/install_base.sh
@@ -1,15 +1,17 @@
 #
 # Install dependencies
 #
-apt install -y htop vim git sshpass curl wget 
+apt install -y htop vim git sshpass ansible curl wget 
 
 
 #
 # Setup NFS
 #
 apt install -y nfs-common
-mkdir /mnt/kubernetes
-echo "10.1.1.202:/volume1/kubernetes /mnt/kubernetes nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+#mkdir /mnt/market_place
+#echo "10.1.1.202:/volume1/market_place /mnt/market_place nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+#mkdir /mnt/kubernetes
+#echo "10.1.1.202:/volume1/kubernetes /mnt/kubernetes nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
 
 
 #
@@ -26,14 +28,27 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 
 
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 groupadd docker
 gpasswd -a root docker
 gpasswd -a cluster docker
+
+
+
+#
+# Install kubernetes
+#
+apt install -y gnupg2
+apt -y install curl apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update
+apt -y install kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
 
 
 #
@@ -43,29 +58,12 @@ sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
 sudo modprobe overlay
 sudo modprobe br_netfilter
-
-
-
-
-
-
-
-echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
-
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
-
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
-
 sudo sysctl --system
 sudo rm /etc/containerd/config.toml
 sudo systemctl restart containerd
-
-
-
-
 reboot now
