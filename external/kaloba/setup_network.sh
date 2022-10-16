c7c25e8f5207fcb4d93ee457b290339bd45dbb88
jodafons <jodafons@lps.ufrj.br>
2022-10-15 21:05:12 -0300

    update

--- external/kaloba/setup_network.sh
+++ external/kaloba/setup_network.sh
@@ -1,6 +1,16 @@
 
 hostname=$1
-ip=$2
+nodenumber=$2
+
+
+#
+# cluster as sudo with no password
+#
+
+usermod -aG sudo $USER
+echo "cluster ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/cluster
+apt install -y net-tools
+
 
 #
 # change hostname
@@ -17,21 +27,11 @@ ff02::2 ip6-allrouters
 hostnamectl
 
 
-
-# get name with ifconfig 
-usermod -aG sudo $USER
-echo "cluster ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/cluster
-apt install net-tools
-
-
-apt install -y vim resolvconf
-# For LPS this should be 146.164.147.2
-echo 'nameserver 146.164.147.2
-search lps.ufrj.br' > /etc/resolvconf/resolv.conf.d/head
-service resolvconf restart
-
-
+#
+# Change IP
+#
 echo "
+# This file describes the network interfaces available on your system
 # and how to activate them. For more information, see interfaces(5).
 
 source /etc/network/interfaces.d/*
@@ -40,24 +40,27 @@ source /etc/network/interfaces.d/*
 auto lo
 iface lo inet loopback
 
+# The primary network interface
 auto ens18
 iface ens18 inet static
-  address 146.164.147.$ip
-  netmask 255.255.255.0
-  gateway 146.164.147.1
-  dns-nameservers 146.164.147.2 8.8.8.8 8.8.8.4
-" > /etc/network/interfaces
+        address 10.1.1.$nodenumber/24
+        gateway 10.1.1.1
+        dns-nameservers 146.164.147.2 8.8.8.8 8.8.8.4
+"> /etc/network/interfaces
 
-
+#netplan apply
 systemctl restart networking
-ifconfig
 
 
 #
-# Add some panic contraints in case of fail
+# resolv conf
 #
-echo "vm.panic_on_oom=1   ;enables panic on OOM">>/etc/sysctl.conf
-echo "kernel.panic=10     ;tells the kernel to reboot ten seconds after panicking">>/etc/sysctl.conf
+apt install -y resolvconf
+# For LPS this should be 146.164.147.2
+echo 'nameserver 146.164.147.2
+search lps.ufrj.br' > /etc/resolvconf/resolv.conf.d/head
+service resolvconf restart
+
 
 
 reboot now
