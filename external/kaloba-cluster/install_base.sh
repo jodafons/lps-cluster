e1c92b4f526a547d5c0d6dd878e3213e189bd2d8
jodafons <jodafons@lps.ufrj.br>
2022-10-16 20:02:58 -0300

    update

--- external/kaloba-cluster/install_base.sh
+++ external/kaloba-cluster/install_base.sh
@@ -38,17 +38,22 @@ gpasswd -a root docker
 gpasswd -a cluster docker
 
 
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
 
 #
 # Install kubernetes
 #
-apt install -y gnupg2
-apt -y install curl apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-apt update
-apt -y install kubelet kubeadm kubectl
-apt-mark hold kubelet kubeadm kubectl
+#apt install -y gnupg2
+#apt -y install curl apt-transport-https
+#curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+#echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#apt update
+#apt -y install kubelet kubeadm kubectl
+#apt-mark hold kubelet kubeadm kubectl
 
 
 #
