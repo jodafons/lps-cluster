5b5eb1fcb0ae31d5df5c8e64c9487f172f7e99d8
jodafons <jodafons@lps.ufrj.br>
2022-10-15 21:55:18 -0300

    update

--- servers/auth-server/dump_kerberos.sh
+++ servers/auth-server/dump_kerberos.sh
@@ -1,5 +1,5 @@
 sudo kdb5_util dump -verbose lps.ufrj.br.krb5
 
-chgrp cluster lps.ufrj.br.krb5
-chown cluster lps.ufrj.br.krb5
+sudo chgrp cluster lps.ufrj.br.krb5
+sudo chown cluster lps.ufrj.br.krb5
 sudo rm lps.ufrj.br.krb5.dump_ok
