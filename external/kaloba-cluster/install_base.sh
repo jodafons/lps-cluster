80048175845f3ea8f5871f9ffc6b87a0431e26ca
jodafons <jodafons@lps.ufrj.br>
2022-10-15 23:00:00 -0300

    update

--- external/kaloba-cluster/install_base.sh
+++ external/kaloba-cluster/install_base.sh
@@ -43,14 +43,29 @@ sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 sudo swapoff -a
 sudo modprobe overlay
 sudo modprobe br_netfilter
-sudo tee /etc/sysctl.d/kubernetes.conf<<EOF
+
+
+
+
+
+
+
+echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
-net.ipv4.ip_forward = 1
 EOF
+
 sudo sysctl --system
 sudo rm /etc/containerd/config.toml
 sudo systemctl restart containerd
+
+
 
 
 reboot now
