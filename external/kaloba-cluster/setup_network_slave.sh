0ad32ff349aa5b09c0bd5cc771fa83388158f109
jodafons <jodafons@lps.ufrj.br>
2022-10-18 18:32:03 -0300

    update

--- external/kaloba-cluster/setup_network_slave.sh
+++ external/kaloba-cluster/setup_network_slave.sh
@@ -49,7 +49,7 @@ iface lo inet loopback
 # The primary network interface
 auto ens18
 iface ens18 inet static
-        address 10.1.1.$nodenumber
+        address 10.1.1.$nodenumber/24
         gateway 10.1.1.1
         dns-nameservers 146.164.147.2 8.8.8.8 8.8.8.4
 "> /etc/network/interfaces
