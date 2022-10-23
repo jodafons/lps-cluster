364d95be5d44b8def3ff90b868eff846ddd003d6
jodafons <jodafons@lps.ufrj.br>
2022-10-23 18:57:29 -0300

    update

--- nodes/preinstall_nvidia.sh
+++ nodes/preinstall_nvidia.sh
@@ -1,3 +1,12 @@
+#
+# Fix kernel
+#
+apt install -y linux-headers-$(uname -r)
+
+#
+# Disabe novauo
+#
+
 echo "blacklist nouveau
 options nouveau modeset=0
 " > /etc/modprobe.d/blacklist-nouveau.conf
