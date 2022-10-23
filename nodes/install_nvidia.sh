8b1c2b84d9e7104cfd32ab3f56da8800d5a76f1a
jodafons <jodafons@lps.ufrj.br>
2022-10-23 19:21:43 -0300

    update

--- nodes/install_nvidia.sh
+++ nodes/install_nvidia.sh
@@ -47,7 +47,7 @@ echo "Copying cudnn to expected path"
 echo ""
 
 cp -r /mnt/market_place/nvidia/deps/cudnn-8.2.2/ /usr/local/
-cp -r /mnt/market_place/nvidia/deps/cudnn-8.1.1/cuda/include/cudnn* /usr/local/cuda-11.7/include
+cp -r /mnt/market_place/nvidia/deps/cudnn-8.2.2/cuda/include/cudnn* /usr/local/cuda-11.7/include
 cp -r /mnt/market_place/nvidia/deps/cudnn-8.2.2/cuda/lib64/libcudnn* /usr/local/cuda-11.7/lib64
 sudo chmod a+r /usr/local/cuda-11.7/include/cudnn* /usr/local/cuda-11.7/lib64/libcudnn*
 
