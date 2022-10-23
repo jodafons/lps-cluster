a3c7f009892fe32a04d17dd2ec41e1948c2d1226
jodafons <jodafons@lps.ufrj.br>
2022-10-23 19:32:24 -0300

    update

--- nodes/setup_nvidia.sh
+++ nodes/setup_nvidia.sh
@@ -11,5 +11,26 @@ echo "blacklist nouveau
 options nouveau modeset=0
 " > /etc/modprobe.d/blacklist-nouveau.conf
 
+
+apt install -y gnupg2
+sudo apt-key adv --fetch-keys  http://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64/3bf863cc.pub
+sudo bash -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/debian11/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+
+sudo apt update
+
+apt install -y nvidia-modprobe
+echo ""
+echo "Starting by installing NVIDIA drivers..."
+echo ""
+
+
+sudo apt update -y && sudo apt upgrade -y
+
+echo ""
+echo "Now installing CUDA versions..."
+echo ""
+
+sudo apt install -y cuda-toolkit-11-7
+
 echo "You need to reboot!"
 #reboot now
