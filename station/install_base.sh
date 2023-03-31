c95558dd100bada935330b060b38f405c3a87a96
jodafons <jodafons@lps.ufrj.br>
2023-03-31 20:06:48 -0300

    update

--- station/install_base.sh
+++ station/install_base.sh
@@ -61,6 +61,18 @@ apt install -y xrdp
 systemctl enable xrdp
 
 
+#
+# Install pyenv
+#
+apt install -y curl
+
+curl https://pyenv.run | bash
+echo ''
+echo '# setup pyenv' >> /etc/bash.bashrc
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> /etc/bash.bashrc
+echo 'eval "$(pyenv init --path)"' >> /etc/bash.bashrc
+
+
 
 sudo reboot now
 
--- station/install_docker.sh
+++ station/install_docker.sh
@@ -23,3 +23,8 @@ sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+sudo groupadd docker
+sudo usermod -aG docker cluster
+
+sudo reboot now
+
