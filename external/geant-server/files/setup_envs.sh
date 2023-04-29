f5726db36470631845e0167025fc9effe34caea9
João Victor da Fonseca Pinto <jodafons@lps.ufrj.br>
2023-04-28 23:47:38 -0300

    Update setup_envs.sh

--- external/geant-server/files/setup_envs.sh
+++ external/geant-server/files/setup_envs.sh
@@ -11,30 +11,8 @@ source /physics/root/build/bin/thisroot.sh
 
 # geant4
 echo "setup geant..."
-
 cd /physics/geant/build
-
-geant4_envbindir=$(pwd)
-#-----------------------------------------------------------------------
-# Setup Geant4 binary and library paths...
-#
-export PATH="$geant4_envbindir":${PATH}
-export LD_LIBRARY_PATH="`cd $geant4_envbindir/BuildProducts/li* > /dev/null ; pwd`":${LD_LIBRARY_PATH}
-
-
-# - Datasets
-export G4NEUTRONHPDATA="`cd $geant4_envbindir/data/G4NDL4.6 > /dev/null ; pwd`"
-export G4LEDATA="`cd $geant4_envbindir/data/G4EMLOW8.0 > /dev/null ; pwd`"
-export G4LEVELGAMMADATA="`cd $geant4_envbindir/data/PhotonEvaporation5.7 > /dev/null ; pwd`"
-export G4RADIOACTIVEDATA="`cd $geant4_envbindir/data/RadioactiveDecay5.6 > /dev/null ; pwd`"
-export G4PARTICLEXSDATA="`cd $geant4_envbindir/data/G4PARTICLEXS4.0 > /dev/null ; pwd`"
-export G4PIIDATA="`cd $geant4_envbindir/data/G4PII1.3 > /dev/null ; pwd`"
-export G4REALSURFACEDATA="`cd $geant4_envbindir/data/RealSurface2.2 > /dev/null ; pwd`"
-export G4SAIDXSDATA="`cd $geant4_envbindir/data/G4SAIDDATA2.0 > /dev/null ; pwd`"
-export G4ABLADATA="`cd $geant4_envbindir/data/G4ABLA3.1 > /dev/null ; pwd`"
-export G4INCLDATA="`cd $geant4_envbindir/data/G4INCL1.0 > /dev/null ; pwd`"
-export G4ENSDFSTATEDATA="`cd $geant4_envbindir/data/G4ENSDFSTATE2.3 > /dev/null ; pwd`"
-
+source geant4.sh
 
 
 
