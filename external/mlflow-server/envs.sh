9c2b939c6dc707756d9f39477d187014477be566
cluster <cluster@slurm-server.lps.ufrj.br>
2023-11-08 09:09:47 -0300

    update

---
+++ external/mlflow-server/envs.sh
@@ -0,0 +1,13 @@
+
+
+
+export MINIO_STORAGE_PATH="/mnt/mlflow_data/minio"
+export DATABASE_STORAGE_PATH="/mnt/mlflow_data/database"
+
+export MYSQL_DATABASE="mlflow"
+export MYSQL_USER="cluster"
+export MYSQL_PASSWORD="mlflow"
+export MYSQL_ROOT_PASSWORD="mlflow"
+
+export MINIO_ROOT_USER="cluster"
+export MINIO_ROOT_PASSWORD="minio"
