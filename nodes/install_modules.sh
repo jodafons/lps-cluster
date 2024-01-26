75630469b39d3ca2ed9ec8aa70c4505584dcb92f
jodafons <jodafons@lps.ufrj.br>
2024-01-26 12:07:53 -0300

    update

--- nodes/install_modules.sh
+++ nodes/install_modules.sh
@@ -10,7 +10,6 @@ fi
 
 
 basepath=$PWD
-cd /tmp
 
 # Install dependencies
 apt install -y tcl8.6-dev
@@ -18,15 +17,12 @@ apt install -y tcl8.6-dev
 # Install modules
 #wget https://github.com/cea-hpc/modules/releases/download/v5.1.1/modules-5.1.1.tar.gz
 #tar xfz modules-5.1.1.tar.gz
-
+cd /tmp
 curl -LJO https://github.com/cea-hpc/modules/releases/download/v5.3.1/modules-5.3.1.tar.gz
 tar xfz modules-5.3.1.tar.gz
-
 cd modules-5.3.1
-./configure --prefix=/usr/share/Modules \
-              --modulefilesdir=/etc/modulefiles
-make
-make install
+./configure --prefix=/usr/share/Modules --modulefilesdir=/etc/modulefiles
+make && make install
 
 ln -s /usr/share/Modules/init/profilif.sh /etc/profile.d/modules.sh
 #ln -s /usr/share/Modules/init/profile.csh /etc/profile.d/modules.csh
@@ -35,5 +31,4 @@ source /usr/share/Modules/init/bash
 # setup modules
 #mkdir /etc/modulefiles/cuda
 #cp modules/cuda/11.4 /etc/modulefiles/cuda
-
 cd $basepath
