710886a94fccc5d5fbb111699a346f6d2b861f83
jodafons <jodafons@lps.ufrj.br>
2024-01-28 22:30:03 -0300

    update

--- nodes/install_cuda.sh
+++ nodes/install_cuda.sh
@@ -1,10 +1,5 @@
 
 
-
-cp files/nvidia /etc/init.d
-chmod 775 /etc/init.d/nvidia
-update-rc.d nvidia defaults
-
 #
 # Disabe novauo
 #
@@ -24,26 +19,28 @@ echo ""
 echo "Install CUDA..."
 echo ""
 
-wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo dpkg -i cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
-sudo cp /var/cuda-repo-debian11-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
-sudo add-apt-repository contrib
-sudo apt-get update
-sudo apt-get -y install cuda
-rm cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
-
+#wget https://developer.download.nvidia.com/compute/cuda/12.1.0/local_installers/cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
+#sudo dpkg -i cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
+#sudo cp /var/cuda-repo-debian11-12-1-local/cuda-*-keyring.gpg /usr/share/keyrings/
+#sudo add-apt-repository contrib
+#sudo apt-get update
+#sudo apt-get -y install cuda
+#rm cuda-repo-debian11-12-1-local_12.1.0-530.30.02-1_amd64.deb
+sudo apt-get -y install cuda-toolkit-12-1
 
 
 #ln -s /mnt/market_place/nvidia/deps/cudnn-8.9.7.29_cuda12/ /usr/local/
 ln -s /mnt/market_place/nvidia/deps/cudnn-8.9.7.29_cuda12/cuda/include/cudnn* /usr/local/cuda/include
-ln -s /mnt/market_place/nvidia/deps/cudnn-8.9.7.29_cuda12/cuda/lib64/libcudnn* /usr/local/cuda/lib64
+ln -s /mnt/market_place/nvidia/deps/cudnn-8.9.7.29_cuda12/cuda/lib/libcudnn* /usr/local/cuda/lib64
 #sudo chmod a+r /usr/local/cuda/include/cudnn* /usr/local/cuda/lib64/libcudnn*
 
 echo ""
 echo "checking all versions..."
 echo ""
 nvidia-smi
-nvcc --version
+#nvcc --version
 
-
+cp files/nvidia /etc/init.d
+chmod 775 /etc/init.d/nvidia
+update-rc.d nvidia defaults
 
