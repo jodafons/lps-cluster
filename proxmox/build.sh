e52b4b175a74d0e8123405f279e2d160db7bda58
cluster <cluster@slurm-server.lps.ufrj.br>
2024-02-20 08:07:49 -0300

    update

--- proxmox/build.sh
+++ proxmox/build.sh
@@ -33,18 +33,19 @@ basepath=$REPO/proxmox/scripts
 #
 # NOTE: script, master_key, physical_node, image_tag, vmname, vmid, last_two_digits_ip, snapshot_name, hosts_path
 #source restore_vm.sh $password caloba-v13 $image caloba51 251 51 base hosts
-source restore_vm.sh $password caloba-v14 $image caloba52 252 52 base hosts
-source restore_vm.sh $password caloba-v15 $image caloba53 253 53 base hosts
-source restore_vm.sh $password caloba-v16 $image caloba54 254 54 base hosts
+#source restore_vm.sh $password caloba-v14 $image caloba52 252 52 base hosts
+#source restore_vm.sh $password caloba-v15 $image caloba53 253 53 base hosts
+#source restore_vm.sh $password caloba-v16 $image caloba54 254 54 base hosts
 
 
-#source restore_vm.sh $password caloba-v11 $image caloba94 294 94 base hosts
-#source restore_vm.sh $password caloba-v11 $image caloba95 295 95 base hosts
 
-#source restore_vm.sh $password caloba-v09 $image caloba90 290 90 base hosts
-#source restore_vm.sh $password caloba-v09 $image caloba91 291 91 base hosts
-#source restore_vm.sh $password caloba-v10 $image caloba92 292 92 base hosts
-#source restore_vm.sh $password caloba-v10 $image caloba93 293 93 base hosts
+source restore_vm.sh $password caloba-v09 $image caloba90 290 90 base hosts
+source restore_vm.sh $password caloba-v09 $image caloba91 291 91 base hosts
+source restore_vm.sh $password caloba-v10 $image caloba92 292 92 base hosts
+source restore_vm.sh $password caloba-v10 $image caloba93 293 93 base hosts
+source restore_vm.sh $password caloba-v11 $image caloba94 294 94 base hosts
+source restore_vm.sh $password caloba-v11 $image caloba95 295 95 base hosts
+source restore_vm.sh $password caloba-v12 $image caloba96 296 96 base hosts
 
 
 
