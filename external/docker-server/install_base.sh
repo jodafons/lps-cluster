88325392e214bee09eb3258bdc6b4a2ed0d81e11
jodafons <jodafons@lps.ufrj.br>
2024-07-08 10:16:24 -0300

    up

--- external/docker-server/install_base.sh
+++ external/docker-server/install_base.sh
@@ -1,56 +1,19 @@
+#
+# Install dependencies
+#
+apt install -y htop vim git sshpass curl wget 
 
 
 #
-# Add some panic contraints in case of fail
+# Setup NFS
 #
-echo "vm.panic_on_oom=1   ;enables panic on OOM">>/etc/sysctl.conf
-echo "kernel.panic=10     ;tells the kernel to reboot ten seconds after panicking">>/etc/sysctl.conf
-
-
-#
-# Append market_place into the mount
-#
-apt install -y nfs-common htop sshpass
-
-
-# install kerberos
-apt install -y krb5-config krb5-user
-apt install -y libpam-krb5
-kadmin -q "addprinc -policy service -randkey host/$HOSTNAME.lps.ufrj.br"
-kadmin -q "ktadd -k /etc/krb5.keytab host/$HOSTNAME.lps.ufrj.br"
-
-
-
-# install LDAP
-apt install -y libnss-ldap
-apt install -y libnss-ldapd
-/etc/init.d/nscd restart
-/etc/init.d/ssh restart
-echo "session required pam_mkhomedir.so skel=/etc/skel/ umask=022" >> /etc/pam.d/common-session
-getent passwd
-
-# configure SSH
-cp files/sshd_config /etc/ssh
-invoke-rc.d ssh restart
-
-
-# configure NTP
-cp files/timesyncd.conf /etc/systemd/
-timedatectl set-ntp true
-timedatectl status
-
-
-# Configure home folder
-echo "10.1.1.202:/volume1/homes /home nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
-mkdir /etc/pam_scripts
-chmod -R 700 /etc/pam_scripts
-chown -R root:root /etc/pam_scripts
-cp files/login-logger.sh /etc/pam_scripts
-chmod +x /etc/pam_scripts/login-logger.sh
-echo "session required pam_exec.so /etc/pam_scripts/login-logger.sh" >> /etc/pam.d/sshd
-
-
-
+apt install -y nfs-common
+mkdir /mnt/market_place
+mkdir /mnt/brics_data
+echo "10.1.1.202:/volume1/market_place /mnt/market_place nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+echo "10.1.1.203:/volume1/brics_data /mnt/brics_data nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+echo "10.1.1.203:/volume1/cern_data /mnt/cern_data nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
+echo "10.1.1.203:/volume1/sonar_data /mnt/sonar_data nfs rsize=32768,wsize=32768,bg,sync,nolock 0 0" >> /etc/fstab
 
 #
 # Install docker
@@ -70,11 +33,12 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get update
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose
 groupadd docker
 gpasswd -a root docker
 gpasswd -a cluster docker
-gpasswd -a brics docker
+
+
 
 
 
