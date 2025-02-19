7a745cf68ead27d06e531f5d8a9432dbcdd833aa
cluster <cluster@slurm-server.lps.ufrj.br>
2025-02-18 22:40:17 -0300

    up

--- servers/slurm-worker/setup_network.sh
+++ servers/slurm-worker/setup_network.sh
@@ -31,7 +31,7 @@ hostnamectl
 #
 # Change IP
 #
-net_interface=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
+network_interface=$(ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}')
 
 echo "
 # This file describes the network interfaces available on your system
