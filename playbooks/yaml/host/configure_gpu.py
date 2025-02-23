ddca4c549cf3502e4a5f2e6f8fb9464655747481
jodafons <jodafons@lps.ufrj.br>
2025-02-23 11:01:01 -0300

    update

--- playbooks/yaml/host/configure_gpu.py
+++ playbooks/yaml/host/configure_gpu.py
@@ -18,10 +18,12 @@ gpu_codes = [ o.split(' ')[-3] for o in gpu_codes.split('\n')]
 gpu_codes = ','.join(gpu_codes)
 GRUB_CMDLINE_LINUX_DEFAULT=f"GRUB_CMDLINE_LINUX_DEFAULT=quiet intel_iommu=on pcie_acs_override=downstream,multifunction initcall_blacklist=sysfb_init video=vesa:off vfio-pci.ids={gpu_codes} vfio_iommu_type1.allow_unsafe_interrupts=1 kvm.ignore_msrs=1 modprobe.blacklist=radeon,nouveau,nvidia,nvidiafb,nvidia-gpu"
 print(GRUB_CMDLINE_LINUX_DEFAULT)
-replace(f"{os.getcwd()}/grub" , "GRUB_CMDLINE_LINUX_DEFAULT", GRUB_CMDLINE_LINUX_DEFAULT)
+replace(f"/etc/default/grub" , "GRUB_CMDLINE_LINUX_DEFAULT", GRUB_CMDLINE_LINUX_DEFAULT)
+
+os.system("update-grub")
 
 modules = ["vfio","vfio_iommu_type1","vfio_pci","vfio_virqfd"]
 with open("/etc/modules",'w') as f:
     for module in modules:
         f.write(f"{module}\n")
-    
+os.system("update-initramfs -u -k all")
